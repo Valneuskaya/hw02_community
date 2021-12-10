cd5d5f71c40f1b07f1b4c08e62902a7d3eb62602
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-10 23:57:30 +0300

    checked after autotests" after tests

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,10 +6,7 @@ def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
     title = "Это главная страница проекта Yatube"
-    context = {
-       'posts': posts,
-       'title': title,
-    }
+    context = {'posts': posts, 'title': title, }
     return render(request, template, context)
 
 
@@ -18,9 +15,5 @@ def group_posts(request, slug):
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
     title = "Здесь будет информация о группах проекта Yatube"
-    context = {
-       'group': group,
-       'posts': posts,
-       'title': title,
-    }
+    context = {'group': group, 'posts': posts, 'title': title, }
     return render(request, template, context)
