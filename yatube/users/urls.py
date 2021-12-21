d6b80e43ce01fb0b8de5d1290e1015924a3b54f1
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-21 17:14:06 +0300

    template login

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,10 +1,22 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from django.urls import path
+from . import views
+
 
 app_name = 'users'
 
 urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/',
+         LoginView.as_view(template_name='users/login.html'),
+         name='login'
+         ),
     path('logout/',
          LogoutView.as_view(template_name='users/logged_out.html'),
-         name='logout'),
+         name='logout',
+         ),
+    path('password_reset/',
+         PasswordResetView.as_view(template_name='users/password_reset_form'),
+         name='password_reset',
+         )
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +123,8 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+
+# Login/logout urls
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index'
