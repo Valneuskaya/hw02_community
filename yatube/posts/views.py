88569af9977f6a39eb824d8b4f32a5ea65f9cc98
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-22 22:05:02 +0300

    added paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,26 @@
+from django.core.paginator import Paginator
+
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = 'posts/index.html'
-    context = {'posts': posts, }
+    context = {'posts': posts, 'page_obj': page_obj, }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.group_posts.all()[:10]
+    posts = group.group_posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     template = 'posts/group_list.html'
-    context = {'group': group, 'posts': posts, }
+    context = {'group': group, 'posts': posts, 'page_obj': page_obj, }
     return render(request, template, context)
