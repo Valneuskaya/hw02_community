dd71599a274364426427af278e44bbc096d7fb02
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-22 15:38:07 +0300

    password reset added

--- yatube/users/models.py
+++ yatube/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Password(models.Model):
+    passw = models.CharField('Password', max_length=200)
+    new_passw1 = models.CharField('New password', max_length=200)
+    new_passw2 = models.CharField('Repet your password', max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Password'
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -6,7 +6,9 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/',
+         views.SignUp.as_view(),
+         name='signup'),
     path('login/',
          LoginView.as_view(template_name='users/login.html'),
          name='login'
@@ -15,8 +17,12 @@ urlpatterns = [
          LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout',
          ),
-    path('password_reset/',
-         PasswordResetView.as_view(template_name='users/password_reset_form'),
-         name='password_reset',
+    path('password_reset_form/', PasswordResetView.as_view(
+         template_name='users/password_reset_form.html'),
+         name='password_reset_form',
+         ),
+    path('password_reset_done/', PasswordResetView.as_view(
+         template_name='users/password_reset_done.html'),
+         name='password_reset_done',
          )
 ]
