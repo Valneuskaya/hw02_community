45b38a65f1670cd676abbb37505e2a7f56646e41
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-25 22:34:41 +0300

    about app

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,8 +20,8 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
+    path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('auth/', include('users.urls', namespace='users')),
-    path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls'))
 ]
