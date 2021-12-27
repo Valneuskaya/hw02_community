20377db4942e270337167bfd41531caf6bb6cdcf
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-27 18:39:25 +0300

    profile.html done

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 
 from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -23,4 +23,40 @@ def group_posts(request, slug):
     page_obj = paginator.get_page(page_number)
     template = 'posts/group_list.html'
     context = {'group': group, 'posts': posts, 'page_obj': page_obj, }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    user = get_object_or_404(User, username=username)
+    posts = user.group_posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    template = 'posts/profile.html'
+    title = 'Профайл пользователя ' + username
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+        'posts': posts,
+        'user': user,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    group = get_object_or_404(Post, pk=post_id)
+    posts = group.group_posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    title = 'Пост '
+    template = 'posts/post_detail.html'
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+        'posts': posts,
+        'group': group,
+    }
     return render(request, template, context)
